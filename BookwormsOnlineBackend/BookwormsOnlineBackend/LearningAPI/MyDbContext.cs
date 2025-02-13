a49fce6855e98611bc1408dbb6f0f3b79fa1a2bf
modermix <lesong4025@gmail.com>
2025-02-14 04:15:21 +0800

    settings

---
+++ BookwormsOnlineBackend/BookwormsOnlineBackend/LearningAPI/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookwormsOnline.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookwormsOnline
+{
+    public class MyDbContext(IConfiguration configuration) : DbContext
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string? connectionString = _configuration.GetConnectionString("MyConnection");
+            if (connectionString != null)
+            {
+                optionsBuilder.UseMySQL(connectionString);
+            }
+        }
+
+        public required DbSet<Tutorial> Tutorials { get; set; }
+
+        public required DbSet<User> Users { get; set; }
+    }
+}
---
+++ BookwormsOnlineFrontend/BookwormsOnlineFrontend/src/themes/MyTheme.js
@@ -0,0 +1,15 @@
+// https://mui.com/material-ui/customization/color/
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#F3E4F5',
+        },
+        secondary: {
+            main: '#f4511e',
+        }
+    }
+});
+
+export default theme;
